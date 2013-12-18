4880a7d26447b624583b0613e0b5da2ccd38db25
marcusoh <marcus.ohara1015@gmail.com>
2013-12-18 10:38:43 -0500

    12/18/13
    updated gitignore

---
+++ New folder/Assets/GUI Stuff/GUIManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+	public GUIText RedHealth;
+	public GUIText GreenHealth;
+	public int RedLives = 6;
+	public int GreenLives = 6;
+	// Use this for initialization
+	void Start () {
+		RedHealth.text = "Health: " + RedLives;
+		GreenHealth.text ="Health: " + GreenLives;
+		RedHealth.enabled = true;
+		GreenHealth.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
